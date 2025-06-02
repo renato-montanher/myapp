1c3d79a6544a6fa245f78e331b831e7c1af6d912
Renato Montanher <renatomontanher.rm@gmail.com>
2025-06-02 14:50:22 -0300

    Adicionado exemplo com age

--- lib/ParsingAutomatico/Person.dart
+++ lib/ParsingAutomatico/Person.dart
@@ -2,12 +2,15 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'Person.g.dart';
 
+//dart run build_runner build --delete-conflicting-outputs
+
 @JsonSerializable()
 class Person {
   /// The generated code assumes these values exist in JSON.
   final String name, lastName;
+  final int age;
 
-  Person({required this.name, required this.lastName});
+  Person({required this.name, required this.lastName, required this.age});
 
   /// Connect the generated [_$PersonFromJson] function to the `fromJson`
   /// factory.
--- lib/ParsingAutomatico/Person.g.dart
+++ lib/ParsingAutomatico/Person.g.dart
@@ -6,10 +6,14 @@ part of 'Person.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-Person _$PersonFromJson(Map<String, dynamic> json) =>
-    Person(name: json['name'] as String, lastName: json['lastName'] as String);
+Person _$PersonFromJson(Map<String, dynamic> json) => Person(
+      name: json['name'] as String,
+      lastName: json['lastName'] as String,
+      age: (json['age'] as num).toInt(),
+    );
 
 Map<String, dynamic> _$PersonToJson(Person instance) => <String, dynamic>{
-  'name': instance.name,
-  'lastName': instance.lastName,
-};
+      'name': instance.name,
+      'lastName': instance.lastName,
+      'age': instance.age,
+    };
